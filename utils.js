d74284bb912325a43ff6fed064cb8ac5999f87b5
mozluk <160273088+mozluk@users.noreply.github.com>
2025-10-31 16:34:48 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T13:34:30.642Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-31T13:34:48.855Z
+Commit: Patch security vulnerability in authentication
